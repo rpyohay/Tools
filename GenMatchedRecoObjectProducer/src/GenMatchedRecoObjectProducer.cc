62af615775cb4713cb409d6ad1f6af288298e2f7
Kyle <kmtos@ucdavis.edu>
2016-02-04 13:35:18 +0100

    Test for Kyle origin Tools branch

--- GenMatchedRecoObjectProducer/src/GenMatchedRecoObjectProducer.cc
+++ GenMatchedRecoObjectProducer/src/GenMatchedRecoObjectProducer.cc
@@ -87,6 +87,9 @@ private:
   //dR matching cut
   double dR_;
 
+  //If wanting to make the ValueMaps for CleanJets Analysis
+  bool ifTauColl_;
+
   //minimum number of gen objects passing cuts that must be found for event to pass filter
   unsigned int minNumGenObjectsToPassFilter_;
 };
@@ -113,11 +116,18 @@ GenMatchedRecoObjectProducer<T>::GenMatchedRecoObjectProducer(const edm::Paramet
   applyPTCuts_(iConfig.getParameter<bool>("applyPTCuts")),
   countKShort_(iConfig.getParameter<bool>("countKShort")),
   dR_(iConfig.getParameter<double>("dR")),
-  minNumGenObjectsToPassFilter_
-  (iConfig.getParameter<unsigned int>("minNumGenObjectsToPassFilter"))
+  ifTauColl_(iConfig.getParameter<bool>("ifTauColl")),
+  minNumGenObjectsToPassFilter_(iConfig.getParameter<unsigned int>("minNumGenObjectsToPassFilter"))
 {
   //register your products
   produces<edm::RefVector<std::vector<T> > >();
+  if (ifTauColl_)
+  {
+    produces<std::vector<T> >( "IgnoreThis" );
+    produces<edm::ValueMap<int> >( "TauMatchedMap" );
+    produces<edm::ValueMap<int> >( "TauDecayModeMap" );
+    produces<edm::ValueMap<double> >( "TauVisiblePtMap" );
+  }//if ifTauColl_
 
   //now do what ever other initialization is needed
   
@@ -142,6 +152,7 @@ GenMatchedRecoObjectProducer<T>::~GenMatchedRecoObjectProducer()
 template<class T>
 bool GenMatchedRecoObjectProducer<T>::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+  std::cout << "<-------------------New GenMatchedRecoObjectProducer--------------------------->" << std::endl;
   //get base gen particles
   edm::Handle<reco::GenParticleCollection> pGenParticles;
   iEvent.getByLabel(genParticleTag_, pGenParticles);
@@ -160,79 +171,125 @@ bool GenMatchedRecoObjectProducer<T>::filter(edm::Event& iEvent, const edm::Even
 
   //fill STL container of pointers to reco objects
   std::vector<T*> recoObjPtrs;
-  for (typename edm::RefVector<std::vector<T> >::const_iterator iRecoObj = pRecoObjs->begin(); 
-       iRecoObj != pRecoObjs->end(); ++iRecoObj) {
+  for (typename edm::RefVector<std::vector<T> >::const_iterator iRecoObj = pRecoObjs->begin(); iRecoObj != pRecoObjs->end(); ++iRecoObj) 
     recoObjPtrs.push_back(const_cast<T*>(iRecoObj->get()));
-  }
-
   //fill STL container of selected gen objects
   std::vector<GenTauDecayID> selectedGenObjs;
-  for (unsigned int iGenParticle = 0; iGenParticle < pSelectedGenParticles->size(); 
-       ++iGenParticle) {
-    try {
+  for (unsigned int iGenParticle = 0; iGenParticle < pSelectedGenParticles->size(); ++iGenParticle) 
+  {
+    try 
+    {
       const reco::GenParticleRef& genParticleRef = pSelectedGenParticles->at(iGenParticle);
-      GenTauDecayID tauDecay(genTauDecayIDPSet_, pGenParticles, 
-			     genParticleRef.key()); /*this now assumes that if iGenParticle refers 
-						      to a tau, it's a status 2 tau*/
+      GenTauDecayID tauDecay(genTauDecayIDPSet_, pGenParticles, genParticleRef.key()); /*this now assumes that if iGenParticle refers to a tau, it's a status 2 tau*/
 
-      /*if matching to gen taus: scalar --> status 23 tau --> status 2 tau --> 
-	status 1/2 tau decay products ==> the gen particle associated to the GenTauDecayID object 
-	is the status 2 tau*/
-      if (((fabs(genParticleRef->pdgId()) == GenTauDecayID::TAUPDGID) && 
-	   tauDecay.isStatus2DecayProduct()) || 
-
-	  /*if matching to gen non-taus: status whatever mother --> status whatever non-gen tau ==> 
-	    the gen particle associated to the GenTauDecayID object should be the gen non-tau 
-	    itself*/
-	  ((fabs(genParticleRef->pdgId()) != GenTauDecayID::TAUPDGID) && 
-	   tauDecay.hasRightMother())) selectedGenObjs.push_back(tauDecay);
-    }
+      /*if matching to gen taus: scalar --> status 23 tau --> status 2 tau --> status 1/2 tau decay products ==> the gen particle associated to the GenTauDecayID object is the status 2 tau*/
+      if (((fabs(genParticleRef->pdgId()) == GenTauDecayID::TAUPDGID) && tauDecay.isStatus2DecayProduct()) || ((fabs(genParticleRef->pdgId()) != GenTauDecayID::TAUPDGID) && tauDecay.hasRightMother()))
+	selectedGenObjs.push_back(tauDecay);
+      /* ^^ if matching to gen non-taus: status whatever mother --> status whatever non-gen tau ==> the gen particle associated to the GenTauDecayID object should be the gen non-tau itself*/
+    }//try
     catch (std::string& ex) { throw cms::Exception("GenMatchedRecoObjectProducer") << ex; }
-  }
+  }//for iGenParticle
 
   /*find the decay type of each gen object if it's a tau (needed to get the visible 
     4-vector), otherwise use the gen particle's 4-vector*/
-  for (std::vector<GenTauDecayID>::iterator iGenObj = selectedGenObjs.begin(); 
-       iGenObj != selectedGenObjs.end(); ++iGenObj) {
+std::cout << "selectedGenObjs->size()= " <<  selectedGenObjs.size() << std::endl;
+  for (std::vector<GenTauDecayID>::iterator iGenObj = selectedGenObjs.begin(); iGenObj != selectedGenObjs.end(); ++iGenObj) 
+  {
     try { iGenObj->tauDecayType(applyPTCuts_, countKShort_); }
     catch (std::string& ex) { throw cms::Exception("GenObjectProducer") << ex; }
-  }
+  }//for iGenObj
 
   //declare pointer to output collection to produce
-  std::auto_ptr<edm::RefVector<std::vector<T> > > 
-    genMatchedRecoObjs(new edm::RefVector<std::vector<T> >);
+  std::auto_ptr<edm::RefVector<std::vector<T> > > genMatchedRecoObjs(new edm::RefVector<std::vector<T> >);
+  std::auto_ptr<std::vector<T> > SetOfTaus ( new std::vector<T> ); //Only Needed for ifTauColl_ 
+  std::vector<int>   genMatchedRecoObjs_Matched; //Only Needed for ifTauColl_ 
+  std::vector<int>    genMatchedRecoObjs_DecayMode; //Only Needed for ifTauColl_
+  std::vector<double> genMatchedRecoObjs_VisiblePt; //Only Needed for ifTauColl_
+std::cout << "ifTauColl_= " << ifTauColl_ << std::endl;
+  if (ifTauColl_)
+  {
+    for (typename std::vector<T>::const_iterator iRecoObj = pBaseRecoObjs->begin(); iRecoObj != pBaseRecoObjs->end(); ++iRecoObj) 
+    {
+      SetOfTaus->push_back(*iRecoObj);
+      genMatchedRecoObjs_Matched.push_back(0);
+      genMatchedRecoObjs_DecayMode.push_back(-1);
+      genMatchedRecoObjs_VisiblePt.push_back(-1.0);
+    }//for iRecoObj
+  }//if ifTauColl_
 
   // //debug
   std::vector<edm::Ref<std::vector<T> > > recoObjsToSave;
 
   //loop over selected gen particles
-  for (std::vector<GenTauDecayID>::iterator iGenObj = selectedGenObjs.begin(); 
-       iGenObj != selectedGenObjs.end(); ++iGenObj) {
-
+  for (std::vector<GenTauDecayID>::iterator iGenObj = selectedGenObjs.begin(); iGenObj != selectedGenObjs.end(); ++iGenObj) 
+  {
     //make a dummy LeafCandidate and ref out of the visible 4-vector
     reco::LeafCandidate::LorentzVector visibleGenP4 = iGenObj->getVisibleTauP4();
-    std::vector<reco::LeafCandidate> 
-      visibleGenParticle(1, reco::LeafCandidate(0.0, visibleGenP4));
+    std::vector<reco::LeafCandidate>   visibleGenParticle(1, reco::LeafCandidate(0.0, visibleGenP4));
     edm::Ref<std::vector<reco::LeafCandidate> > visibleGenParticleRef(&visibleGenParticle, 0);
 
     //find the nearest reco object to the gen particle
     int nearestRecoObjKey = -1;
-    const T* nearestRecoObj = 
-      Common::nearestObject(visibleGenParticleRef, recoObjPtrs, nearestRecoObjKey);
+    const T* nearestRecoObj = Common::nearestObject(visibleGenParticleRef, recoObjPtrs, nearestRecoObjKey);
 
     //if nearest reco object is within dR_ of the gen object, save
-    if ((nearestRecoObj != NULL) && (reco::deltaR(*nearestRecoObj, *visibleGenParticleRef) < dR_)) {
-	genMatchedRecoObjs->
-	  push_back(edm::Ref<std::vector<T> >(pBaseRecoObjs, 
-					      pRecoObjs->at(nearestRecoObjKey).key()));
-    }
-  }
-
+    if ((nearestRecoObj != NULL) && (reco::deltaR(*nearestRecoObj, *visibleGenParticleRef) < dR_)) 
+    {
+      genMatchedRecoObjs->push_back(edm::Ref<std::vector<T> >(pBaseRecoObjs, pRecoObjs->at(nearestRecoObjKey).key()));
+      std::cout << "\tFound GenRecoMatch" << std::endl;
+      if (ifTauColl_)
+      {
+	int iter = 0;
+	edm::Ref<std::vector<T> > RecoTau = edm::Ref<std::vector<T> >(pBaseRecoObjs, pRecoObjs->at(nearestRecoObjKey).key());
+        for (typename std::vector<T>::const_iterator iRecoObj = SetOfTaus->begin(); iRecoObj != SetOfTaus->end(); ++iRecoObj)
+        {
+          double dPhi = reco::deltaPhi(iRecoObj->phi(), RecoTau->phi() ), dEta = iRecoObj->eta() - RecoTau->eta(), dRTaus = sqrt( dPhi*dPhi + dEta*dEta );
+	  std::cout << "\tRecoTau->pt= " << RecoTau->pt() << "  \tiRecoObj->pt= " << iRecoObj->pt() << "  \tdRTaus= " << dRTaus << std::endl;
+          if (dRTaus < .1 && abs(iRecoObj->pt() - RecoTau->pt() ) < 1)
+          {
+std::cout << "\t\titer= " << iter << "  iRecoObj - SetOfTaus->begin()= " <<  iRecoObj - SetOfTaus->begin() << std::endl;
+	    std::cout << "\t\t<-------FOUND IN SetOfTaus------------>" << std::endl;
+            genMatchedRecoObjs_Matched[iter] = 1;
+	    std::pair<reco::PFTau::hadronicDecayMode, GenTauDecayID::DecayType> pair = iGenObj->tauDecayType(applyPTCuts_, countKShort_);
+            genMatchedRecoObjs_DecayMode[iter] = std::get<1>(pair);
+            genMatchedRecoObjs_VisiblePt[iter] = visibleGenP4.Pt();
+          }//dRTaus
+	  iter++;
+        }//for iRecoObj
+      }//if ifTauColl_
+    }//if nearestRecoObj
+  }//for iGenObj
   //flag indicating whether right number of gen-matched reco objects were found
   bool foundGenMatchedRecoObject = genMatchedRecoObjs->size() >= minNumGenObjectsToPassFilter_;
 
   iEvent.put(genMatchedRecoObjs); //this function frees the auto_ptr argument
 
+  if (ifTauColl_)
+  {
+    const edm::OrphanHandle<std::vector<T> > SetOfRecoTausProd = iEvent.put( SetOfTaus, "IgnoreThis"  );
+
+    std::auto_ptr<edm::ValueMap<int> > TauMatchedMap(new edm::ValueMap<int>());
+    edm::ValueMap<int>::Filler int1Filler(*TauMatchedMap);
+    for (unsigned int i = 0; i< genMatchedRecoObjs_Matched.size(); ++i)
+      std::cout << "genMatchedRecoObjs_Matched[" << i << "]= " << genMatchedRecoObjs_Matched[i] << std::endl;
+    int1Filler.insert(SetOfRecoTausProd, genMatchedRecoObjs_Matched.begin(), genMatchedRecoObjs_Matched.end() );
+    int1Filler.fill();
+    iEvent.put(TauMatchedMap, "TauMatchedMap" );
+
+    std::auto_ptr<edm::ValueMap<int> > TauDecayModeMap(new edm::ValueMap<int>());
+    edm::ValueMap<int>::Filler int2Filler(*TauDecayModeMap);
+    int2Filler.insert(SetOfRecoTausProd, genMatchedRecoObjs_DecayMode.begin(), genMatchedRecoObjs_DecayMode.end());
+    int2Filler.fill();
+    iEvent.put(TauDecayModeMap, "TauDecayModeMap" );
+
+    std::auto_ptr<edm::ValueMap<double> > TauVisiblePtMap(new edm::ValueMap<double>());
+    edm::ValueMap<double>::Filler doubleFiller(*TauVisiblePtMap);
+    doubleFiller.insert(SetOfRecoTausProd, genMatchedRecoObjs_VisiblePt.begin(), genMatchedRecoObjs_VisiblePt.end());
+    doubleFiller.fill();
+    iEvent.put(TauVisiblePtMap, "TauVisiblePtMap" );
+
+  }//ifTauColl_
+  std::cout << "<-------------------End GenMatchedRecoObjectProducer--------------------------->" << std::endl;
   //stop processing if no gen-matched objects were found
   return foundGenMatchedRecoObject;
 }
