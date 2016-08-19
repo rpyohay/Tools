e659c48ca53223439e3aaa83d445fd8d6414e3c6
Rachel <Rachel.Yohay@cern.ch>
2016-08-19 22:53:40 +0200

    added HLT validation

--- GenAnalyzer/plugins/GenObjectAnalyzer.cc
+++ GenAnalyzer/plugins/GenObjectAnalyzer.cc
@@ -5,7 +5,7 @@
 // 
 /**\class GenObjectAnalyzer GenObjectAnalyzer.cc Tools/GenMatchedRecoObjectProducer/src/GenObjectAnalyzer.cc
 
- Description: debug tau gen tools
+ Description: debug tau gen tools and MC generation features
 
  Implementation:
      [Notes on implementation]
@@ -30,11 +30,17 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "DataFormats/Math/interface/deltaR.h"
 
 #include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
 #include "Tools/Common/interface/GenTauDecayID.h"
 
 #include "TH1D.h"
+#include "TH2D.h"
+
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "DataFormats/HLTReco/interface/TriggerEvent.h"
+#include "Tools/Common/interface/Common.h"
 //
 // class declaration
 //
@@ -61,11 +67,23 @@ class GenObjectAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>
       // ----------member data ---------------------------
 
   edm::EDGetTokenT<reco::GenParticleCollection> genParticleTag_;
-  edm::EDGetTokenT<reco::GenParticleRefVector> selectedGenParticleTag_;
+  edm::EDGetTokenT<reco::GenParticleRefVector> selectedGenTauTag_;
+  edm::EDGetTokenT<reco::GenParticleRefVector> selectedGenMuonTag_;
+  edm::EDGetTokenT<reco::GenParticleRefVector> selectedGenPseudoscalarTag_;
 
   edm::ParameterSet genTauDecayIDPSet_;
 
-  std::map<std::string, TH1D*> hists_;
+  edm::EDGetTokenT<edm::TriggerResults> triggerBitsTag_;
+  edm::EDGetTokenT<trigger::TriggerEvent> triggerEventTag_;
+
+  std::map<std::string, TH1D*> hists1D_;
+  std::map<std::string, TH2D*> hists2D_;
+
+  unsigned int nTriggerableEvts_;
+
+  double diTauDRMax_;
+  double diMuonDRMax_;
+  double mu1PTMax_;
 };
 
 //
@@ -83,10 +101,25 @@ GenObjectAnalyzer::GenObjectAnalyzer(const edm::ParameterSet& iConfig) :
 
   genParticleTag_(consumes<reco::GenParticleCollection>
 		  (iConfig.getParameter<edm::InputTag>("genParticleTag"))),
-  selectedGenParticleTag_(consumes<reco::GenParticleRefVector>
-			  (iConfig.getParameter<edm::InputTag>("selectedGenParticleTag"))),
+  selectedGenTauTag_(consumes<reco::GenParticleRefVector>
+		     (iConfig.getParameter<edm::InputTag>("selectedGenTauTag"))),
+  selectedGenMuonTag_(consumes<reco::GenParticleRefVector>
+		     (iConfig.getParameter<edm::InputTag>("selectedGenMuonTag"))),
+  selectedGenPseudoscalarTag_(consumes<reco::GenParticleRefVector>
+			      (iConfig.getParameter<edm::InputTag>("selectedGenPseudoscalarTag"))),
 
-  genTauDecayIDPSet_(iConfig.getParameter<edm::ParameterSet>("genTauDecayIDPSet"))
+  genTauDecayIDPSet_(iConfig.getParameter<edm::ParameterSet>("genTauDecayIDPSet")),
+
+  triggerBitsTag_(consumes<edm::TriggerResults>
+		  (iConfig.getParameter<edm::InputTag>("triggerBitsTag"))),
+  triggerEventTag_(consumes<trigger::TriggerEvent>
+		   (iConfig.getParameter<edm::InputTag>("triggerEventTag"))),
+
+  nTriggerableEvts_(0),
+
+  diTauDRMax_(0),
+  diMuonDRMax_(0),
+  mu1PTMax_(0)
 {
    //now do what ever initialization is needed
    usesResource("TFileService");
@@ -111,26 +144,245 @@ GenObjectAnalyzer::~GenObjectAnalyzer()
 void
 GenObjectAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+
+  // Get collections from event //
+
   edm::Handle<reco::GenParticleCollection> pGenParticles;
   iEvent.getByToken(genParticleTag_, pGenParticles);
 
-  edm::Handle<reco::GenParticleRefVector> pSelectedGenParticles;
-  iEvent.getByToken(selectedGenParticleTag_, pSelectedGenParticles);
+  edm::Handle<reco::GenParticleRefVector> pSelectedGenTaus;
+  iEvent.getByToken(selectedGenTauTag_, pSelectedGenTaus);
 
-  for (reco::GenParticleRefVector::const_iterator iSelectedGenParticle = 
-	 pSelectedGenParticles->begin(); iSelectedGenParticle != pSelectedGenParticles->end(); 
-       ++iSelectedGenParticle) {
+  edm::Handle<reco::GenParticleRefVector> pSelectedGenMuons;
+  iEvent.getByToken(selectedGenMuonTag_, pSelectedGenMuons);
+
+  edm::Handle<reco::GenParticleRefVector> pSelectedGenPseudoscalars;
+  iEvent.getByToken(selectedGenPseudoscalarTag_, pSelectedGenPseudoscalars);
+
+  edm::Handle<edm::TriggerResults> triggerBits;
+  iEvent.getByToken(triggerBitsTag_, triggerBits);
+
+  edm::Handle<trigger::TriggerEvent> triggerEvent;
+  iEvent.getByToken(triggerEventTag_, triggerEvent);
+
+  // Analyze decay properties and kinematics //
+
+  reco::LeafCandidate::LorentzVector fourBodyP4;
+  reco::LeafCandidate::LorentzVector diTauP4;
+  reco::LeafCandidate::LorentzVector diMuonP4;
+  std::vector<reco::LeafCandidate> taus;
+  // std::vector<reco::GenParticleRef> taus;
+  unsigned int nLeptons = 0;
+  unsigned int nTaus = 0;
+  unsigned int nMuons = 0;
+
+  for (reco::GenParticleRefVector::const_iterator iSelectedGenTau = pSelectedGenTaus->begin(); 
+       iSelectedGenTau != pSelectedGenTaus->end(); ++iSelectedGenTau) {
     try {
 
-      GenTauDecayID tauDecay(genTauDecayIDPSet_, pGenParticles, iSelectedGenParticle->key());
+      GenTauDecayID tauDecay(genTauDecayIDPSet_, pGenParticles, iSelectedGenTau->key());
       const std::pair<reco::PFTau::hadronicDecayMode, GenTauDecayID::DecayType> decayType = 
 	tauDecay.tauDecayType(false, false);
 
-      hists_["genDecayMode"]->Fill(decayType.first);
+      hists1D_["genDecayMode"]->Fill(decayType.first);
+
+      reco::LeafCandidate::LorentzVector visibleGenP4 = tauDecay.getVisibleTauP4();
+      fourBodyP4+=visibleGenP4;
+      diTauP4+=visibleGenP4;
+      taus.push_back(reco::LeafCandidate(0.0, visibleGenP4));
 
     }
     catch (std::string& ex) { throw cms::Exception("GenObjectAnalyzer") << ex; }
+
+    // fourBodyP4+=(*iSelectedGenTau)->p4();
+    // diTauP4+=(*iSelectedGenTau)->p4();
+    // taus.push_back(*iSelectedGenTau);
+    ++nLeptons;
+    ++nTaus;
   }
+
+  std::vector<reco::GenParticleRef> muons;
+  std::vector<reco::GenParticle*> muonPtrs;
+  double mu1PT = 0.0;
+  double mu1Eta = 0.0;
+  double mu1Phi = 0.0;
+
+  for (reco::GenParticleRefVector::const_iterator iSelectedGenMuon = pSelectedGenMuons->begin(); 
+       iSelectedGenMuon != pSelectedGenMuons->end(); ++iSelectedGenMuon) {
+
+    fourBodyP4+=(*iSelectedGenMuon)->p4();
+    diMuonP4+=(*iSelectedGenMuon)->p4();
+    muons.push_back(*iSelectedGenMuon);
+    muonPtrs.push_back(const_cast<reco::GenParticle*>(iSelectedGenMuon->get()));
+    ++nLeptons;
+    ++nMuons;
+
+    if ((*iSelectedGenMuon)->pt() > mu1PT) {
+      mu1PT = (*iSelectedGenMuon)->pt();
+      mu1Eta = (*iSelectedGenMuon)->eta();
+      mu1Phi = (*iSelectedGenMuon)->phi();
+    }
+
+  }
+
+  for (reco::GenParticleRefVector::const_iterator iSelectedGenPseudoscalar = 
+	 pSelectedGenPseudoscalars->begin(); iSelectedGenPseudoscalar != 
+	 pSelectedGenPseudoscalars->end(); ++iSelectedGenPseudoscalar) {
+
+    hists1D_["genPseudoscalarEta"]->Fill((*iSelectedGenPseudoscalar)->eta());
+
+    if (nTaus == 0) {
+      for (reco::GenParticleRefVector::const_iterator iDaughter = 
+	     (*iSelectedGenPseudoscalar)->daughterRefVector().begin(); iDaughter != 
+	     (*iSelectedGenPseudoscalar)->daughterRefVector().end(); ++iDaughter) {
+	std::cout << "Daughter PDG ID: " << (*iDaughter)->pdgId() << std::endl;
+      }
+
+    }
+  }
+
+  if (nLeptons == 4) hists1D_["gen4BodyMass"]->Fill(fourBodyP4.M());
+  if (nTaus == 2) {
+    hists1D_["genDiTauMass"]->Fill(diTauP4.M());
+    // hists1D_["genDiTauDRBeforeHLT"]->Fill(std::sqrt(deltaR2(*(taus[0]), *(taus[1]))));
+    const double diTauDR = std::sqrt(deltaR2(taus[0], taus[1]));
+    hists1D_["genDiTauDRBeforeHLT"]->Fill(diTauDR);
+    if (diTauDR > diTauDRMax_) diTauDRMax_ = diTauDR;
+  }
+  if (nMuons == 2) {
+    hists1D_["genDiMuonMass"]->Fill(diMuonP4.M());
+    const double diMuonDR = std::sqrt(deltaR2(*(muons[0]), *(muons[1])));
+    hists1D_["genDiMuonDRBeforeHLT"]->Fill(diMuonDR);
+    if (diMuonDR > diMuonDRMax_) diMuonDRMax_ = diMuonDR;
+  }
+  hists1D_["nLeptons"]->Fill(nLeptons);
+  hists1D_["nTaus"]->Fill(nTaus);
+  hists1D_["nMuons"]->Fill(nMuons);
+
+  hists1D_["genMu1PT"]->Fill(mu1PT);
+  hists1D_["genMu1Eta"]->Fill(mu1Eta);
+  hists2D_["genMu1AbsEtaVsPT"]->Fill(mu1PT, fabs(mu1Eta));
+
+  if (mu1PT > mu1PTMax_) mu1PTMax_ = mu1PT;
+
+  for (reco::GenParticleCollection::const_iterator iGenParticle = pGenParticles->begin(); 
+       iGenParticle != pGenParticles->end(); ++iGenParticle) {
+
+    if (iGenParticle->pdgId() == GenTauDecayID::HPDGID) {
+      hists1D_["genHiggsEta"]->Fill(iGenParticle->eta());
+    }
+
+  }
+
+  // Analyze trigger decision //
+
+  bool passMu45Eta2p1 = false;
+  bool passMu17Mu8SameSignDZ = false;
+  bool passTripleMu12105 = false;
+  const edm::TriggerNames &names = iEvent.triggerNames(*triggerBits);
+  for (unsigned int iBit = 0; iBit < triggerBits->size(); ++iBit) {
+    if (names.triggerName(iBit).find("HLT_Mu45_eta2p1") != std::string::npos) {
+      passMu45Eta2p1 = triggerBits->accept(iBit);
+    }
+    if (names.triggerName(iBit).find("HLT_Mu17_Mu8_SameSign_DZ") != std::string::npos) {
+      passMu17Mu8SameSignDZ = triggerBits->accept(iBit);
+    }
+    if (names.triggerName(iBit).find("HLT_TripleMu_12_10_5") != std::string::npos) {
+      passTripleMu12105 = triggerBits->accept(iBit);
+    }
+  }
+  if (passMu45Eta2p1) hists1D_["HLTDecisionByPath"]->Fill(0);
+  else if (passMu17Mu8SameSignDZ) hists1D_["HLTDecisionByPath"]->Fill(1);
+  else if (passTripleMu12105) hists1D_["HLTDecisionByPath"]->Fill(2);
+  else hists1D_["HLTDecisionByPath"]->Fill(3);
+
+  if ((mu1PT > 45.0/*GeV*/) && (fabs(mu1Eta) < 2.1)) {
+    ++nTriggerableEvts_;
+
+    if (nTaus == 2) {
+      // hists1D_["genDiTauDRAfterHLT"]->Fill(std::sqrt(deltaR2(*(taus[0]), *(taus[1]))));
+      hists1D_["genDiTauDRAfterHLT"]->Fill(std::sqrt(deltaR2(taus[0], taus[1])));
+    }
+    if (nMuons == 2) {
+      hists1D_["genDiMuonDRAfterHLT"]->Fill(std::sqrt(deltaR2(*(muons[0]), *(muons[1]))));
+    }
+    bool HLTDecision = false;
+    const edm::TriggerNames &names = iEvent.triggerNames(*triggerBits);
+    for (unsigned int iBit = 0; iBit < triggerBits->size(); ++iBit) {
+      if (names.triggerName(iBit).find("HLT_Mu45_eta2p1") != std::string::npos) {
+    	HLTDecision = triggerBits->accept(iBit);
+    	hists1D_["HLTDecision"]->Fill(HLTDecision);
+      }
+    }
+
+    double mu2PT = 0.0;
+    double mu2Eta = 0.0;
+    double mu2Phi = 0.0;
+    if (mu1PT == muons[0]->pt()) {
+      mu2PT = muons[1]->pt();
+      mu2Eta = muons[1]->eta();
+      mu2Phi = muons[1]->phi();
+    }
+    else {
+      mu2PT = muons[0]->pt();
+      mu2Eta = muons[0]->eta();
+      mu2Phi = muons[0]->phi();
+    }
+    std::cerr << "\nmu1\n";
+    std::cerr << "pT (GeV): " << mu1PT << " | eta: " << mu1Eta << " | phi: " << mu1Phi << std::endl;
+    std::cerr << "mu2\n";
+    std::cerr << "pT (GeV): " << mu2PT << " | eta: " << mu2Eta << " | phi: " << mu2Phi << std::endl;
+    const trigger::TriggerObjectCollection& triggerObjects(triggerEvent->getObjects());
+    const trigger::size_type 
+      iFilter(triggerEvent->filterIndex
+    	      (edm::InputTag("hltL3fL1sMu16orMu25L1f0L2f10QL3Filtered45e2p1Q", "", "HLT")));
+    // const trigger::size_type 
+    //   iFilter(triggerEvent->filterIndex
+    // 	      (edm::InputTag("hltL2fL1sMu16orMu25L1f0L2Filtered10Q", "", "HLT")));
+    unsigned int nMu1MatchedTrgObjs = 0;
+    unsigned int nMu2MatchedTrgObjs = 0;
+    if (iFilter < triggerEvent->sizeFilters()) {
+      const trigger::Keys& HLTKeys(triggerEvent->filterKeys(iFilter));
+      std::cerr << "HLT_Mu45_eta2p1 trigger objects\n";
+      for (trigger::Keys::const_iterator iHLTKey = HLTKeys.begin(); iHLTKey != HLTKeys.end(); 
+    	   ++iHLTKey) {
+    	const trigger::TriggerObject& triggerObject(triggerObjects[*iHLTKey]);
+    	std::cerr << "ID: " << triggerObject.id() << " | pT (GeV): " << triggerObject.pt();
+    	std::cerr << " | eta: " << triggerObject.eta() << " | phi: " << triggerObject.phi();
+    	std::cerr << std::endl;
+    	int nearestGenMuKey = -1;
+    	const reco::Particle triggerObjectParticle(triggerObject.particle());
+    	const reco::GenParticle* nearestGenMu = 
+    	  Common::nearestObject(triggerObjectParticle, muonPtrs, nearestGenMuKey);
+    	const double trgObjNearestGenMuonDR = 
+    	  std::sqrt(deltaR2(triggerObjectParticle, *nearestGenMu));
+    	hists1D_["trgObjNearestGenMuonDR"]->Fill(trgObjNearestGenMuonDR);
+    	if (trgObjNearestGenMuonDR < 0.1) {
+    	  if (nearestGenMu->pt() == mu1PT) ++nMu1MatchedTrgObjs;
+    	  if (nearestGenMu->pt() == mu2PT) ++nMu2MatchedTrgObjs;
+    	}
+      }
+      std::cerr << std::endl;
+    }
+    else std::cerr << "Event did not pass HLT_Mu45_eta2p1\n\n";
+    if (HLTDecision) {
+      if ((nMu1MatchedTrgObjs > 0) && (nMu2MatchedTrgObjs > 0)) {
+    	hists1D_["HLTDecisionByGenMuon"]->Fill(0);
+      }
+      if ((nMu1MatchedTrgObjs > 0) && (nMu2MatchedTrgObjs == 0)) {
+    	hists1D_["HLTDecisionByGenMuon"]->Fill(1);
+      }
+      if ((nMu1MatchedTrgObjs == 0) && (nMu2MatchedTrgObjs > 0)) {
+    	hists1D_["HLTDecisionByGenMuon"]->Fill(2);
+      }
+      if ((nMu1MatchedTrgObjs == 0) && (nMu2MatchedTrgObjs == 0)) {
+    	hists1D_["HLTDecisionByGenMuon"]->Fill(3);
+      }
+    }
+    else hists1D_["HLTDecisionByGenMuon"]->Fill(4);
+
+  }
+
 }
 
 
@@ -140,12 +392,12 @@ GenObjectAnalyzer::beginJob()
 {
   edm::Service<TFileService> fileService;
 
-  hists_["genDecayMode"] = 
+  hists1D_["genDecayMode"] = 
     fileService->make<TH1D>("genDecayMode", "", 
 			    reco::PFTau::kThreeProngNPiZero - reco::PFTau::kOneProng0PiZero + 1, 
 			    reco::PFTau::kOneProng0PiZero - 0.5, 
 			    reco::PFTau::kThreeProngNPiZero + 0.5);
-  TAxis* xAxis = hists_["genDecayMode"]->GetXaxis();
+  TAxis* xAxis = hists1D_["genDecayMode"]->GetXaxis();
   xAxis->SetBinLabel(reco::PFTau::kOneProng0PiZero + 1, "1-prong + 0-#pi^{0}");
   xAxis->SetBinLabel(reco::PFTau::kOneProng1PiZero + 1, "1-prong + 1-#pi^{0}");
   xAxis->SetBinLabel(reco::PFTau::kOneProng2PiZero + 1, "1-prong + 2-#pi^{0}");
@@ -161,12 +413,71 @@ GenObjectAnalyzer::beginJob()
   xAxis->SetBinLabel(reco::PFTau::kThreeProng2PiZero + 1, "3-prong + 2-#pi^{0}");
   xAxis->SetBinLabel(reco::PFTau::kThreeProng3PiZero + 1, "3-prong + 3-#pi^{0}");
   xAxis->SetBinLabel(reco::PFTau::kThreeProngNPiZero + 1, "3-prong + N-#pi^{0}");
+
+  hists1D_["gen4BodyMass"] = 
+    fileService->make<TH1D>("gen4BodyMass", ";m_{#mu#mu#tau#tau} (GeV);", 34, 0.0, 750.0);
+  hists1D_["genDiTauMass"] = 
+    fileService->make<TH1D>("genDiTauMass", ";m_{#tau#tau} (GeV);", 20, 0.0, 10.0);
+  hists1D_["genDiMuonMass"] = 
+    fileService->make<TH1D>("genDiMuonMass", ";m_{#mu#mu} (GeV);", 20, 8.0, 10.0);
+  hists1D_["genDiTauDRBeforeHLT"] = 
+    fileService->make<TH1D>("genDiTauDRBeforeHLT", ";#DeltaR(#tau,#tau);", 35, 0.0, 3.5);
+  hists1D_["genDiMuonDRBeforeHLT"] = 
+    fileService->make<TH1D>("genDiMuonDRBeforeHLT", ";#DeltaR(#mu,#mu);", 35, 0.0, 3.5);
+  hists1D_["nLeptons"] = 
+    fileService->make<TH1D>("nLeptons", ";No. leptons from H decay;", 5, -0.5, 4.5);
+  hists1D_["nTaus"] = fileService->make<TH1D>("nTaus", ";No. taus from H decay;", 3, -0.5, 2.5);
+  hists1D_["nMuons"] = fileService->make<TH1D>("nMuons", ";No. muons from H decay;", 3, -0.5, 2.5);
+  hists1D_["genPseudoscalarEta"] = 
+    fileService->make<TH1D>("genPseudoscalarEta", ";a #eta;", 20, -5.0, 5.0);
+  hists1D_["genHiggsEta"] = 
+    fileService->make<TH1D>("genHiggsEta", ";a #eta;", 20, -5.0, 5.0);
+
+  hists1D_["genMu1PT"] = 
+    fileService->make<TH1D>("genMu1PT", ";#mu_{1} p_{T} (GeV);", 28, 0.0, 560.0);
+  hists1D_["genMu1Eta"] = 
+    fileService->make<TH1D>("genMu1Eta", ";#mu_{1} #eta;", 20, -5.0, 5.0);
+  hists2D_["genMu1AbsEtaVsPT"] = 
+    fileService->make<TH2D>("genMu1AbsEtaVsPT", ";#mu_{1} p_{T} (GeV);#mu_{1} |#eta|", 
+			    28, 0.0, 560.0, 20, -5.0, 5.0);
+
+  hists1D_["HLTDecisionByPath"] = 
+    fileService->make<TH1D>("HLTDecisionByPath", "", 4, -0.5, 3.5);
+  xAxis = hists1D_["HLTDecisionByPath"]->GetXaxis();
+  xAxis->SetBinLabel(1, "(1)"/*"HLT_Mu45_eta2p1"*/);
+  xAxis->SetBinLabel(2, "(2) && !(1)"/*"HLT_Mu17_Mu8_SameSign_DZ && !HLT_Mu45_eta2p1"*/);
+  xAxis->SetBinLabel(3, "(3) && !(1) && !(2)"/*"HLT_TripleMu_12_10_5 && !HLT_Mu45_eta2p1 && !HLT_Mu17_Mu8_SameSign_DZ"*/);
+  xAxis->SetBinLabel(4, "!(1) && !(2) && !(3)"/*"!HLT_TripleMu_12_10_5 && !HLT_Mu45_eta2p1 && !HLT_Mu17_Mu8_SameSign_DZ"*/);
+  hists1D_["genDiTauDRAfterHLT"] = 
+    fileService->make<TH1D>("genDiTauDRAfterHLT", ";#DeltaR(#tau,#tau);", 35, 0.0, 3.5);
+  hists1D_["genDiMuonDRAfterHLT"] = 
+    fileService->make<TH1D>("genDiMuonDRAfterHLT", ";#DeltaR(#mu,#mu);", 35, 0.0, 3.5);
+  hists1D_["HLTDecision"] = fileService->make<TH1D>("HLTDecision", "", 2, -0.5, 1.5);
+  xAxis = hists1D_["HLTDecision"]->GetXaxis();
+  xAxis->SetBinLabel(1, "Fail");
+  xAxis->SetBinLabel(2, "Pass");
+  hists1D_["trgObjNearestGenMuonDR"] = 
+    fileService->make<TH1D>("trgObjNearestGenMuonDR", 
+			    ";#DeltaR(trigger object, nearest gen muon);", 25, 0.0, 0.005);
+  hists1D_["HLTDecisionByGenMuon"] = 
+    fileService->make<TH1D>("HLTDecisionByGenMuon", "", 4, -0.5, 3.5);
+  xAxis = hists1D_["HLTDecisionByGenMuon"]->GetXaxis();
+  xAxis->SetBinLabel(1, "Both");
+  xAxis->SetBinLabel(2, "#mu_{1} only");
+  xAxis->SetBinLabel(3, "#mu_{2} only");
+  xAxis->SetBinLabel(4, "Neither");
+
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
 GenObjectAnalyzer::endJob() 
 {
+  std::cout << std::endl << "Number of triggerable events: " << nTriggerableEvts_ << std::endl;
+  std::cout << std::endl << "Max. di-tau dR: " << diTauDRMax_ << std::endl;
+  std::cout << std::endl << "Max. di-muon dR: " << diMuonDRMax_ << std::endl;
+  std::cout << std::endl << "Max. mu1 pT (GeV): " << mu1PTMax_ << std::endl;
+  std::cout << std::endl;
 }
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
@@ -177,7 +488,11 @@ GenObjectAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions
   edm::ParameterSetDescription desc;
 
   desc.add<edm::InputTag>("genParticleTag", edm::InputTag("genParticles"));
-  desc.add<edm::InputTag>("selectedGenParticleTag", edm::InputTag("genTauHadATauMuTauHadSelector"));
+  desc.add<edm::InputTag>("selectedGenTauTag", edm::InputTag("genTauHadATauMuTauHadSelector"));
+  desc.add<edm::InputTag>("selectedGenMuonTag", edm::InputTag("genAMuMuSelector"));
+  desc.add<edm::InputTag>("selectedGenPseudoscalarTag", edm::InputTag("genAHAASelector"));
+  desc.add<edm::InputTag>("triggerBitsTag", edm::InputTag("TriggerResults", "", "HLT"));
+  desc.add<edm::InputTag>("triggerEventTag", edm::InputTag("hltTriggerSummaryAOD", "", "HLT"));
 
   edm::ParameterSetDescription ATauTauPSet;
   ATauTauPSet.add<std::vector<int> >("momPDGID", std::vector<int>(1, GenTauDecayID::APDGID));
--- GenAnalyzer/test/test.py
+++ GenAnalyzer/test/test.py
@@ -1,6 +1,7 @@
 ### PDG IDs ###
 
 A_PDGID = 36
+H_PDGID = 25
 MU_PDGID = 13
 TAU_PDGID = 15
 ANY_PDGID = 0
@@ -47,6 +48,7 @@ process = cms.Process("TEST")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
+process.MessageLogger.categories.append('TriggerSummaryAnalyzerAOD')
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
     )
@@ -54,31 +56,32 @@ process.options = cms.untracked.PSet(
 ### Input ###
 
 import FWCore.Utilities.FileUtils as FileUtils
-mylist = FileUtils.loadListFromFile('NMSSM_ggH_a9_H1125_H2500_H3500_2mu2tau.txt')
+mylist = FileUtils.loadListFromFile('NMSSM_ggH_a9_H1125_MG5aMCatNLO_2mu2tau_GEN-SIM.txt')
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
     )
 process.source = cms.Source(
     "PoolSource",
-    fileNames = cms.untracked.vstring(*mylist)
+    fileNames = cms.untracked.vstring(*mylist),
+    skipEvents = cms.untracked.uint32(0)
     )
 
-### Select gen tau_had decays from a-->tau_mu+tau_had ###
+### Select gen tau decays from a-->tautau ###
 
-ATauTauPSet = cms.PSet(momPDGID = cms.vint32(A_PDGID),
-                       chargedHadronPTMin = cms.double(0.0), #should always be 0.0
-                       neutralHadronPTMin = cms.double(0.0), #should always be 0.0
-                       chargedLeptonPTMin = cms.double(0.0), #should always be 0.0
-                       totalPTMin = cms.double(0.0)) #should always be 0.0
+APSet = cms.PSet(momPDGID = cms.vint32(A_PDGID),
+                 chargedHadronPTMin = cms.double(0.0), #should always be 0.0
+                 neutralHadronPTMin = cms.double(0.0), #should always be 0.0
+                 chargedLeptonPTMin = cms.double(0.0), #should always be 0.0
+                 totalPTMin = cms.double(0.0))         #should always be 0.0
 
-process.genTauHadATauMuTauHadSelector = cms.EDFilter(
+process.genTauATauTauSelector = cms.EDFilter(
     'GenObjectProducer',
     genParticleTag = cms.InputTag('genParticles'),
     absMatchPDGIDs = cms.vuint32(TAU_PDGID),     #choose a gen tau...
     sisterAbsMatchPDGID = cms.uint32(TAU_PDGID), #...whose sister is another gen tau...
-    genTauDecayIDPSet = ATauTauPSet,             #...and whose mother is a pseudoscalar a
-    primaryTauDecayType = cms.uint32(TAU_HAD),   #primary tau decay mode is had...
-    sisterTauDecayType = cms.uint32(TAU_MU),     #...sister tau decay mode is mu
+    genTauDecayIDPSet = APSet,                   #...and whose mother is a pseudoscalar a
+    primaryTauDecayType = cms.uint32(TAU_ALL),   #primary tau decay mode is anything...
+    sisterTauDecayType = cms.uint32(TAU_ALL),    #...sister tau decay mode is anything
     primaryTauPTRank = cms.int32(ANY_PT_RANK),   #should always be ANY_PT_RANK
     primaryTauHadronicDecayType = cms.int32(TAU_ALL_HAD), #choose TAU_ALL_HAD when the tau decay 
                                                           #type is hadronic and you want any 
@@ -87,31 +90,90 @@ process.genTauHadATauMuTauHadSelector = cms.EDFilter(
                                                           #type is non-hadronic
     primaryTauAbsEtaMax = cms.double(-1.0), #no cut on |eta|
     primaryTauPTMin = cms.double(-1.0),     #no cut on pT
-    countSister = cms.bool(False),          #only put the hadronic tau in the output collection
+    countSister = cms.bool(True),           #put both taus in the output collection
     applyPTCuts = cms.bool(False),          #should always be False
     countKShort = cms.bool(False),          #should always be False
-    minNumGenObjectsToPassFilter = cms.uint32(1), #EDFilter only returns true if >=1 
-                                                  #tau_mu+tau_had is found
+    minNumGenObjectsToPassFilter = cms.uint32(0), #EDFilter only returns true if >=0 taus are 
+                                                  #found
     makeAllCollections = cms.bool(False) #should always be False
     )
 
-### Analyze gen tau_had decays from a-->tau_mu+tau_had ###
+### Select gen muons from a-->mumu ###
+
+process.genMuAMuMuSelector = cms.EDFilter(
+    'GenObjectProducer',
+    genParticleTag = cms.InputTag('genParticles'),
+    absMatchPDGIDs = cms.vuint32(MU_PDGID),      #choose a gen muon...
+    sisterAbsMatchPDGID = cms.uint32(MU_PDGID),  #...whose sister is another gen muon...
+    genTauDecayIDPSet = APSet,                   #...and whose mother is a pseudoscalar a
+    primaryTauDecayType = cms.uint32(TAU_ALL),
+    sisterTauDecayType = cms.uint32(TAU_ALL),
+    primaryTauPTRank = cms.int32(ANY_PT_RANK),   #should always be ANY_PT_RANK
+    primaryTauHadronicDecayType = cms.int32(TAU_ALL_HAD), #choose TAU_ALL_HAD when the primary 
+                                                          #particle is not a tau
+    sisterHadronicDecayType = cms.int32(TAU_ALL_HAD),     #choose TAU_ALL_HAD when the primary 
+                                                          #particle is not a tau
+    primaryTauAbsEtaMax = cms.double(-1.0), #no cut on |eta|
+    primaryTauPTMin = cms.double(-1.0),     #no cut on pT
+    countSister = cms.bool(True),           #put both muons in the output collection
+    applyPTCuts = cms.bool(False),          #should always be False
+    countKShort = cms.bool(False),          #should always be False
+    minNumGenObjectsToPassFilter = cms.uint32(0), #EDFilter only returns true if >=0 muons are 
+                                                  #found
+    makeAllCollections = cms.bool(False) #should always be False
+    )
+
+### Select gen pseudoscalars from H-->aa ###
+
+HPSet = APSet.clone()
+HPSet.momPDGID = cms.vint32(H_PDGID)
+
+process.genAHAASelector = cms.EDFilter(
+    'GenObjectProducer',
+    genParticleTag = cms.InputTag('genParticles'),
+    absMatchPDGIDs = cms.vuint32(A_PDGID),       #choose a gen pseudoscalar...
+    sisterAbsMatchPDGID = cms.uint32(A_PDGID),   #...whose sister is another gen pseudoscalar...
+    genTauDecayIDPSet = HPSet,                   #...and whose mother is a scalar H
+    primaryTauDecayType = cms.uint32(TAU_ALL),
+    sisterTauDecayType = cms.uint32(TAU_ALL),
+    primaryTauPTRank = cms.int32(ANY_PT_RANK),   #should always be ANY_PT_RANK
+    primaryTauHadronicDecayType = cms.int32(TAU_ALL_HAD), #choose TAU_ALL_HAD when the primary 
+                                                          #particle is not a tau
+    sisterHadronicDecayType = cms.int32(TAU_ALL_HAD),     #choose TAU_ALL_HAD when the primary 
+                                                          #particle is not a tau
+    primaryTauAbsEtaMax = cms.double(-1.0), #no cut on |eta|
+    primaryTauPTMin = cms.double(-1.0),     #no cut on pT
+    countSister = cms.bool(True),           #put both pseudoscalars in the output collection
+    applyPTCuts = cms.bool(False),          #should always be False
+    countKShort = cms.bool(False),          #should always be False
+    minNumGenObjectsToPassFilter = cms.uint32(0), #EDFilter only returns true if >=0 pseudoscalars 
+                                                  #are found
+    makeAllCollections = cms.bool(False) #should always be False
+    )
+
+### Analyze gen H-->aa-->2mu2tau events ###
 
 process.genObjectAnalyzer = cms.EDAnalyzer(
     'GenObjectAnalyzer',
     genParticleTag = cms.InputTag('genParticles'),
-    selectedGenParticleTag = cms.InputTag('genTauHadATauMuTauHadSelector'),
-    genTauDecayIDPSet = ATauTauPSet
+    selectedGenTauTag = cms.InputTag('genTauATauTauSelector'),
+    selectedGenMuonTag = cms.InputTag('genMuAMuMuSelector'),
+    selectedGenPseudoscalarTag = cms.InputTag('genAHAASelector'),
+    triggerBitsTag = cms.InputTag("TriggerResults", "", "HLT"),
+    triggerEventTag = cms.InputTag("hltTriggerSummaryAOD", "", "HLT"),
+    genTauDecayIDPSet = APSet
     )
 
 process.TFileService = cms.Service(
     'TFileService',
-    fileName = cms.string('test.root')
+    fileName = cms.string('test_MG5aMCatNLO_H125_GEN-SIM.root')
 )
 
 ### Path ###
 
 process.path = cms.Path(
-    process.genTauHadATauMuTauHadSelector*
+    process.genTauATauTauSelector*
+    process.genMuAMuMuSelector*
+    process.genAHAASelector*
     process.genObjectAnalyzer
     )
